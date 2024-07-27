2c9031967f52cfdce468994a3055e6fd65be56b5
Marcel Masque Salgado <marcel.masque.salgado@prodrive-technologies.com>
2024-07-27 14:21:52 +0200

    Overkill for Day 3 b

--- src/day3.rs
+++ src/day3.rs
@@ -1,8 +1,10 @@
+use core::num;
+
 use itertools::Itertools;
 
 use crate::helpers::read_lines;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq)]
 struct Number {
     value: i32,
     line_num: i32,
@@ -11,43 +13,67 @@ struct Number {
 }
 
 struct Grid {
-    grid: Vec<Vec<char>>,
+    grid: Vec<Vec<Entry>>,
 }
 
+#[derive(Clone)]
+struct Entry {
+    value: char,
+    col_num: i32,
+    line_num: i32,
+    number: Option<Number>, //hacky, but if an entry is a number, we can put it here
+}
+impl Entry {
+    fn new(value: char, col_num: i32, line_num: i32, number: Option<Number>) -> Entry {
+        Entry {
+            value,
+            col_num,
+            line_num,
+            number,
+        }
+    }
+}
 impl Grid {
-    fn get_at(&self, line: i32, col: i32) -> Option<char> {
+    fn get_at(&self, line: i32, col: i32) -> Option<Entry> {
         let line: usize = line.try_into().ok()?;
         let col: usize = col.try_into().ok()?;
-        self.grid.get(line)?.get(col).copied()
+        self.grid.get(line)?.get(col).cloned()
     }
 }
 
-impl Number {
-    fn adjacent_chars(&self, grid: &Grid) -> Vec<char> {
+trait Adjacent {
+    fn adjacent_entries(&self, grid: &Grid) -> Vec<Entry>;
+}
+
+impl Adjacent for Number {
+    fn adjacent_entries(&self, grid: &Grid) -> Vec<Entry> {
         let left = grid.get_at(self.line_num, self.start_col - 1);
         let right = grid.get_at(self.line_num, self.end_col + 1);
         let range_above_below = (self.start_col - 1)..=(self.end_col + 1);
-        let above: Vec<Option<char>> = range_above_below
+        let above: Vec<Option<Entry>> = range_above_below
             .clone()
             .map(|i| grid.get_at(self.line_num - 1, i))
             .collect();
-        let below: Vec<Option<char>> = range_above_below
+        let below: Vec<Option<Entry>> = range_above_below
             .clone()
-            .map(|i| grid.get_at(self.line_num + 1, i))
+            .map(|i| grid.get_at(self.line_num + 1, i).into())
             .collect();
 
         [vec![left, right], above, below]
             .concat()
             .iter()
             .flatten()
-            .copied()
+            .cloned()
             .collect()
     }
-    fn adjacent_symbols(&self, grid: &Grid) -> Vec<char> {
-        self.adjacent_chars(grid)
+}
+
+impl dyn Adjacent {
+    fn adjacent_symbols(&self, grid: &Grid) -> Vec<Entry> {
+        self.adjacent_entries(grid)
             .iter()
-            .filter(|x| x.ne(&&'.') && !x.is_ascii_digit())
-            .copied()
+            .filter(|x| x.value.ne(&&'.') && !x.value.is_ascii_digit())
+            .cloned()
             .collect()
     }
     fn has_adjacent_symbols(&self, grid: &Grid) -> bool {
@@ -55,25 +81,46 @@ impl Number {
     }
 }
 
-pub fn day3a() {
-    let lines = read_lines("inputs/day3.txt");
-    let grid: Vec<Vec<char>> = lines.iter().map(|s| s.chars().collect()).collect();
-    let grid = Grid { grid };
-    // find the numbers in each line and turn them into entries
-    let mut all_numbers = vec![];
-    for (line_num, line) in lines.iter().enumerate() {
-        let numbers: Vec<Vec<(usize, char)>> = line
-            .chars()
-            .enumerate()
-            .chunk_by(|(_, s)| s.is_ascii_digit())
-            .into_iter()
-            .filter(|(k, _)| *k)
-            .map(|(_, chunk)| chunk.collect())
+impl Adjacent for Entry {
+    fn adjacent_entries(&self, grid: &Grid) -> Vec<Entry> {
+        let left = grid.get_at(self.line_num, self.col_num - 1);
+        let right = grid.get_at(self.line_num, self.col_num + 1);
+        let range_above_below = (self.col_num - 1)..=(self.col_num + 1);
+        let above: Vec<Option<Entry>> = range_above_below
+            .clone()
+            .map(|i| grid.get_at(self.line_num - 1, i))
             .collect();
-        for number_vector in numbers {
+        let below: Vec<Option<Entry>> = range_above_below
+            .clone()
+            .map(|i| grid.get_at(self.line_num + 1, i).into())
+            .collect();
+
+        [vec![left, right], above, below]
+            .concat()
+            .iter()
+            .flatten()
+            .cloned()
+            .collect()
+    }
+}
+
+fn add_numbers_to_grid(grid: &mut Grid) -> Vec<Number> {
+    // find the numbers in each line and turn them into entries
+    // also adds the numbers found to the grid entries
+    let mut all_numbers = vec![];
+    for (line_num, line) in grid.grid.iter_mut().enumerate() {
+        let chunks = line
+            .iter_mut()
+            .enumerate()
+            .chunk_by(|(_, s)| s.value.is_ascii_digit());
+        let number_chunks = chunks.into_iter().filter(|(k, _)| *k);
+        let mut numbers: Vec<Vec<(usize, &mut Entry)>> =
+            number_chunks.map(|(_, chunk)| chunk.collect()).collect();
+
+        for number_vector in numbers.iter_mut() {
             let value: i32 = number_vector
                 .iter()
-                .fold("".to_string(), |s, (_, c)| s + &c.to_string())
+                .fold("".to_string(), |s, (_, c)| s + &c.value.to_string())
                 .parse()
                 .unwrap();
             let start_col = *number_vector
@@ -89,13 +136,66 @@ pub fn day3a() {
                 end_col,
                 line_num: line_num as i32,
             };
-            all_numbers.push(number);
+            all_numbers.push(number.clone());
+            // add the number to each entry
+            for entry in number_vector.iter_mut() {
+                entry.1.number = Some(number.clone());
+            }
         }
     }
+    all_numbers
+}
+
+fn build_grid(lines: Vec<String>) -> Grid {
+    Grid {
+        grid: lines
+            .iter()
+            .enumerate()
+            .map(|(i, s)| {
+                s.chars()
+                    .enumerate()
+                    .map(|(j, c)| Entry::new(c, j as i32, i as i32, None))
+                    .collect()
+            })
+            .collect(),
+    }
+}
+
+pub fn day3a() {
+    let lines = read_lines("inputs/day3.txt");
+    let mut grid = build_grid(lines);
+    let all_numbers = add_numbers_to_grid(&mut grid);
     let sum_of_part_numbers = all_numbers
         .iter()
-        .filter(|n| n.has_adjacent_symbols(&grid))
+        .filter(|n| (*n as &dyn Adjacent).has_adjacent_symbols(&grid)) // This is not great
         .fold(0, |c, n| c + n.value);
 
     println!("DAY3A: Sum of part numbers: {}", sum_of_part_numbers);
+}
+
+pub fn day3b() {
+    let lines = read_lines("inputs/day3.txt");
+    let mut grid = build_grid(lines);
+    add_numbers_to_grid(&mut grid);
+
+    let mut total = 0;
+    // now for every *, look at its adjacent entries, and see if they are numbers (with a bit of a trick)
+    for line in &grid.grid {
+        for entry in line {
+            if entry.value == '*' {
+                let adjacents = entry.adjacent_entries(&grid);
+                // hacky: duplicates of numbers will be next to each other, because of how we compute the adjacents
+                let dedupped_numbers: Vec<Number> = adjacents
+                    .iter()
+                    .map(|x| x.number.clone())
+                    .flatten()
+                    .dedup()
+                    .collect();
+                if dedupped_numbers.len() == 2 {
+                    total += dedupped_numbers.iter().fold(1, |a, b| a * b.value);
+                }
+            }
+        }
+    }
+    println!("DAY3B: Sum of gear ratios {}", total);
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod day2;
 mod day3;
 mod helpers;
 
+use day3::day3b;
+
 use crate::day1::{day1a, day1b};
 use crate::day2::{day2a, day2b};
 use crate::day3::day3a;
@@ -13,4 +15,5 @@ fn main() {
     day2a();
     day2b();
     day3a();
+    day3b();
 }
